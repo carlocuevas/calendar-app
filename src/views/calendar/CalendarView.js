cd4fa3e11ea89f52e5588449735fc166201b7d41
Carlo <cuevascarlo07@gmail.com>
2018-12-14 11:52:34 +0800

    Added Dependencies, Libraries, and Architecture/ Initial Commit

---
+++ src/views/calendar/CalendarView.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+
+import BaseMVPView from '../common/base/BaseMVPView'
+import ConnectView from '../../utils/ConnectView'
+import Presenter from './presenter/CalendarPresenter'
+
+class CalendarView extends BaseMVPView {
+  constructor (props) {
+    super(props)
+  }
+}
+
+
+export default CalendarView
